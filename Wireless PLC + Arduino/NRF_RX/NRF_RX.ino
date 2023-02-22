719b2d4f6dbfe4fb18a0ea265cfa8d88c36c4ac3
A Rahman Musyaffa <97543902+Asyaffa@users.noreply.github.com>
2023-02-22 18:47:26 +0700

    Add files via upload

---
+++ Wireless PLC + Arduino/NRF_RX/NRF_RX.ino
@@ -0,0 +1,46 @@
+/*
+    Written by Aqilla Rahman Musyaffa
+*/
+
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24l01.h>
+
+RF24 radio (9, 10);
+
+const byte address [6] = "00001";
+int sensorOptoValue [4];
+
+void setup() 
+{
+    Serial.begin (9600);
+    
+    radio.begin ();
+    radio.openReadingPipe (0, address);
+    radio.setPALevel (RF24_PA_MIN);
+    radio.startListening ();
+}
+
+void loop() 
+{
+    if (radio.available ())
+    {
+        radio.read (sensorOptoValue, sizeof (sensorOptoValue));
+        int PE1Value = sensorOptoValue [0];
+        int IPValue = sensorOptoValue [1];
+        int PE2Value = sensorOptoValue [2];
+        int PE3Value = sensorOptoValue [3];
+
+        Serial.print ("Sensor 1 : ");
+        Serial.print (PE1Value);
+        Serial.print ("\t");
+        Serial.print ("Sensor 2 : ");
+        Serial.print (IPValue);
+        Serial.print ("\t");
+        Serial.print ("Sensor 3 : ");
+        Serial.print (PE2Value);
+        Serial.print ("\t");
+        Serial.print ("Sensor 4 : ");
+        Serial.println (PE3Value);
+    }
+}
---
+++ Wireless PLC + Arduino/README.md
@@ -0,0 +1,4 @@
+# PLC Sensors to Arduino (RF Communication)
+RF-based Communication between Two Arduino
+
+An Arduino connected to nRF24L01 & PLC system sensors (through Optocoupler) and the other one only connected to nRF24L01
